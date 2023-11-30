87bd5134317e37a259b0c0613957b5b7438b36ff
BobW7 <1186717311@qq.com>
2023-11-30 17:16:10 +0800

    权限管理 在路由配置文件，加入meta定义access等权限属性 在全局页面组件中，绑定一个全局路由监听，读取这个access属性并做判断，先判断用户是否有对应的访问权限（用户具有role属性） 如果有，正常跳转，没有，重定向到无权限页面或者登录页

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,22 @@
 </style>
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+
+const router = useRouter();
+const store = useStore();
+
+//路由监听，读取路由中的meta中的access属性（也是自己定义的）
+router.beforeEach((to, from, next) => {
+  //仅管理员可见
+  if (to.meta?.access === "canAdmin") {
+    //判断当前用户是否有权限
+    if (store.state.user.loginUser?.role !== "Admin") {
+      next("/noAuth");
+      return;
+    }
+  }
+  next();
+});
 </script>
--- src/store/user.ts
+++ src/store/user.ts
@@ -6,13 +6,14 @@ export default {
   state: () => ({
     loginUser: {
       userName: "未登录",
+      role: "Common",
     },
   }),
   // actions 执行异步操作，并且触发mutation的更改（调用mutation）
   actions: {
     getLoginUser({ commit, state }, payload) {
       //todo 真实登录的话就要请求后端了，从远程请求获取登录信息
-      commit("updateUser", { userName: "Bob" });
+      commit("updateUser", { userName: "Bob", role: "Admin" });
     },
   },
   // mutations 定义了更新变量增删改的方法
