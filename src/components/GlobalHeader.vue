f3ae089ae2d2c8b9d58d56a77b663371387a3e57
BobW7 <1186717311@qq.com>
2023-11-30 15:17:36 +0800

    解决菜单跳转高亮

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="globalHeader">
-    <a-menu mode="horizontal" theme="light" :default-selected-keys="['1']">
+    <a-menu
+      mode="horizontal"
+      theme="light"
+      :selected-keys="selectedKeys"
+      @menu-item-click="doMenuClick"
+    >
       <a-menu-item
         key="0"
         :style="{ padding: 0, marginRight: '38px' }"
@@ -11,15 +16,32 @@
           <div class="title">BobOJ</div>
         </div>
       </a-menu-item>
-      <a-menu-item key="1">Home</a-menu-item>
-      <a-menu-item key="2">Solution</a-menu-item>
-      <a-menu-item key="3">Cloud Service</a-menu-item>
-      <a-menu-item key="4">Cooperation</a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path">
+        {{ item.name }}
+      </a-menu-item>
     </a-menu>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { routes } from "../router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+//默认主页
+const selectedKeys = ref(["/"]);
+//路由跳转之后，更新选中的菜单项
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
