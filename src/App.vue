b1c7720bf8484ad28be95f3fd5ca1df93ab73b6d
BobW7 <1186717311@qq.com>
2023-12-27 14:03:54 +0800

    新建access\index.ts文件，将原有的路由拦截，权限校验逻辑放在独立的文件夹中，只要不引用，就不会开启，不会对项目有影响

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,7 @@
 </style>
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
 import { onMounted } from "vue";
-
-const router = useRouter();
-const store = useStore();
 
 /**
  * 全局初始化函数，有全局单次调用的代码，都可以写到这里
@@ -26,18 +21,5 @@ const doInit = () => {
 
 onMounted(() => {
   doInit();
-});
-
-//路由监听，读取路由中的meta中的access属性（也是自己定义的）
-router.beforeEach((to, from, next) => {
-  //仅管理员可见
-  if (to.meta?.access === "canAdmin") {
-    //判断当前用户是否有权限
-    if (store.state.user.loginUser?.role !== "Admin") {
-      next("/noAuth");
-      return;
-    }
-  }
-  next();
 });
 </script>
